e71d8320bbd4109a8d8405837a4336dc5b0403f3
Eduardo-Muricy <Eduardo-Muricy@hotmail.com>
2023-03-15 17:49:39 -0300

    Previsao-do-tempo

---
+++ script.js
@@ -0,0 +1,25 @@
+let chave = "557006fc4fc7307abc00a763c728cefe"
+
+
+function colocarNaTela(dados){
+console.log(dados)
+document.querySelector(".cidade").innerHTML ="Tempo em "+ dados.name
+document.querySelector(".temp").innerHTML =Math.floor( dados.main.temp) + " °C"
+document.querySelector(".descricao").innerHTML = dados.weather[0].description
+document.querySelector(".humidade").innerHTML =  "Humidade "+ dados.main.humidity + "%"
+document.querySelector(".icone").src=src="https://openweathermap.org/img/wn/" + dados.weather[0].icon +".png"
+}
+
+async function buscarCidade(cidade){
+   let dados = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=" + chave + "&lang=pt_br"+"&units=metric").then(resposta=> resposta.json()
+   )
+    colocarNaTela(dados)
+  
+}
+
+function cliqueiNoBotao (){
+   let cidade = document.querySelector(".input-cidade").value
+   
+   buscarCidade(cidade)
+}
